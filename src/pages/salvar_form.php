2e63754de3d974df296bf749d539057dfe2bdbe6
Gabriel Gomes <gabrielgones5@gmail.com>
2024-04-29 17:14:03 -0300

    2.0

--- src/pages/salvar_form.php
+++ src/pages/salvar_form.php
@@ -1,5 +1,50 @@
 <?php
+    include('conexao.php');
+    session_start();
+    print_r($_POST);
 
-print_r($_POST)
+    $prg01 = $_POST['prazo'];
+    $prg02 = $_POST['cordial'];
+    $prg03 = $_POST['satisfacao'];
+    $prg04 = $_POST['retorno'];
+
+    print($_SESSION['cpf']);
+
+    $sql_verificar = "SELECT * FROM `cliente` WHERE cpf = {$_SESSION['cpf']}";
+    $result = mysqli_query($conn, $sql_verificar);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            $_SESSION['id'] = $row['Id_cliente'];
+        }
+      }
+
+      $sql_avaliacao = "INSERT INTO `avaliacoes` (`id_cliente`) VALUES ('{$_SESSION['id']}')";
+
+      if (!mysqli_query($conn, $sql_avaliacao)) {
+        echo "Erro ao inserir avaliação: " . mysqli_error($conn);
+    }
+
+    $sql_av = "SELECT id_avaliacao FROM `avaliacoes` WHERE id_cliente = {$_SESSION['id']}";
+    $result = mysqli_query($conn, $sql_av);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            print_r($row);
+            $_SESSION['ID_AV'] = $row['id_avaliacao'];
+        }
+    }
+
+    $sql_resposta_1 = "INSERT INTO `respostas`(`id_cliente`, `id_pergunta`, `id_avaliacao`, `resposta`) VALUES ('{$_SESSION['id']}','1','{$_SESSION['ID_AV']}','$prg01')";
+    $sql_resposta_2 = "INSERT INTO `respostas`(`id_cliente`, `id_pergunta`, `id_avaliacao`, `resposta`) VALUES ('{$_SESSION['id']}','2','{$_SESSION['ID_AV']}','$prg02')";
+    $sql_resposta_3 = "INSERT INTO `respostas`(`id_cliente`, `id_pergunta`, `id_avaliacao`, `resposta`) VALUES ('{$_SESSION['id']}','3','{$_SESSION['ID_AV']}','$prg03')";
+    $sql_resposta_4 = "INSERT INTO `respostas`(`id_cliente`, `id_pergunta`, `id_avaliacao`, `resposta`) VALUES ('{$_SESSION['id']}','4','{$_SESSION['ID_AV']}','$prg04')";
+
+    mysqli_query($conn, $sql_resposta_1);
+    mysqli_query($conn, $sql_resposta_2);
+    mysqli_query($conn, $sql_resposta_3);
+    mysqli_query($conn, $sql_resposta_4);
 
 ?>
--- src/pages/salvar_usuario.php
+++ src/pages/salvar_usuario.php
@@ -1,23 +1,22 @@
 <?php
     include('conexao.php');
+    session_start();
 
     // Recebendo os dados do formulário
     $nome = $_POST['name'];
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
     $CPF = $_POST['CPF'];
-
-    print($CPF);
+    $_SESSION['cpf'] = $CPF;
 
     // Consulta para verificar se já existe um cliente com o mesmo CPF
     $sql_verificar = "SELECT * FROM `cliente` WHERE cpf = $CPF";
     $result_verificar = mysqli_query($conn, $sql_verificar);
-
-    print_r(mysqli_num_rows($result_verificar));
-
     // Verifica se o CPF já existe no banco de dados
+
     if(mysqli_num_rows($result_verificar) > 0){
         // Se já existe, redireciona de volta para o formulário com uma mensagem de erro
+        header('Location: form.php');
         exit; // Encerra o script para evitar a execução do restante do código
     } else {
         // Se não existe, insere o novo cliente no banco de dados
@@ -28,6 +27,7 @@
             die("Error: " . mysqli_error($conn));
         }
         // Redireciona de volta para o formulário após a inserção bem-sucedida
+        header('Location: form.php');
         exit; // Encerra o script para evitar a execução do restante do código
     }
 ?>
