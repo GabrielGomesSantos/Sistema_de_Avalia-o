e46758606bebdfead8a0d83382bbb51ebef963aa
Gabriel Gomes <gabrielgones5@gmail.com>
2024-04-29 16:25:07 -0300

    comited.yan

--- src/pages/salvar_usuario.php
+++ src/pages/salvar_usuario.php
@@ -1,22 +1,33 @@
 <?php
     include('conexao.php');
 
+    // Recebendo os dados do formulário
     $nome = $_POST['name'];
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
     $CPF = $_POST['CPF'];
 
-    $sql = "SELECT * FROM `cliente` WHERE 'cpf' LIKE '$CPF'";
-    $result = mysqli_query($conn,$sql);
+    print($CPF);
 
-    if(mysqli_num_rows($result) != 0){
-        header('Location: form.php');
-    }else{
-        $sql = "INSERT INTO `cliente` (`nome`, `e-mail`, `telefone`, `cpf`) VALUES ('$nome','$email','$telefone','$CPF')";
+    // Consulta para verificar se já existe um cliente com o mesmo CPF
+    $sql_verificar = "SELECT * FROM `cliente` WHERE cpf = $CPF";
+    $result_verificar = mysqli_query($conn, $sql_verificar);
 
-        if (!mysqli_query($conn,$sql)) {
-            die("Error: ".mysqli_error($conn));
+    print_r(mysqli_num_rows($result_verificar));
+
+    // Verifica se o CPF já existe no banco de dados
+    if(mysqli_num_rows($result_verificar) > 0){
+        // Se já existe, redireciona de volta para o formulário com uma mensagem de erro
+        exit; // Encerra o script para evitar a execução do restante do código
+    } else {
+        // Se não existe, insere o novo cliente no banco de dados
+        $sql_inserir = "INSERT INTO `cliente` (`nome`, `e-mail`, `telefone`, `cpf`) VALUES ('$nome','$email','$telefone','$CPF')";
+
+        if (!mysqli_query($conn, $sql_inserir)) {
+            // Se houver um erro ao inserir no banco de dados, exibe uma mensagem de erro
+            die("Error: " . mysqli_error($conn));
         }
-        header('Location: form.php');
+        // Redireciona de volta para o formulário após a inserção bem-sucedida
+        exit; // Encerra o script para evitar a execução do restante do código
     }
 ?>
